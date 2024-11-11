dd8bbdebd14ca5de0c81bf579e1fb770f31e5a85
Alpha <alphagma@gmail.com>
2024-11-10 20:53:04 -0500

    new_value! macro

--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,29 @@ use std::ops::Mul;
 #[derive(Debug)]
 struct Value {
     data: f64,
+    previous: Vec<Value>,
+}
+
+macro_rules! new_value {
+    ($data:expr) => {
+        Value::new($data, vec![])
+    };
+    ($data: expr, $($rest: expr),+) => {
+        Value::new($data, vec![$($rest),+])
+    };
+}
+
+impl Value {
+    fn new(data: f64, previous: Vec<Value>) -> Value {
+        Value { data, previous }
+    }
 }
 
 impl Add for Value {
     type Output = Value;
 
     fn add(self, other: Value) -> Value {
-        Value {
-            data: self.data + other.data,
-        }
+        Value::new(self.data + other.data, vec![self, other])
     }
 }
 
@@ -20,9 +34,7 @@ impl Mul for Value {
     type Output = Value;
 
     fn mul(self, other: Value) -> Value {
-        Value {
-            data: self.data * other.data,
-        }
+        Value::new(self.data * other.data, vec![self, other])
     }
 }
 
@@ -32,22 +44,30 @@ mod tests {
 
     #[test]
     fn can_be_instantiated() {
-        let value = Value { data: 3.0 };
+        let value = new_value![3.0];
         assert_eq!(value.data, 3.0);
     }
 
     #[test]
+    fn can_be_instantiated_with_previous() {
+        let value1 = new_value![3.0];
+        let value2 = new_value![4.0, value1];
+        assert_eq!(value2.data, 4.0);
+        assert_eq!(value2.previous[0].data, 3.0);
+    }
+
+    #[test]
     fn can_add() {
-        let value1 = Value { data: 3.0 };
-        let value2 = Value { data: 4.0 };
+        let value1 = new_value![3.0];
+        let value2 = new_value![4.0];
         let result = value1 + value2;
         assert_eq!(result.data, 7.0);
     }
 
     #[test]
     fn can_multiply() {
-        let value1 = Value { data: 3.0 };
-        let value2 = Value { data: 4.0 };
+        let value1 = new_value![3.0];
+        let value2 = new_value![4.0];
         let result = value1 * value2;
         assert_eq!(result.data, 12.0);
     }
